f1b29b450c840de030d18fabeae1910310f42e7b
Prithvi2011 <76046770+Prithvi2011@users.noreply.github.com>
2021-02-26 13:34:06 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+<html>
+   <head>
+    <title>school website</title>
+<meta name="viewport" content="width=device-width, inital-scale=1">
+    <link rel="stylesheet" herf="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/boostrap/3.4.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" herf="https://cdnjs.cloudflare.com/ajax/libs/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+        
+    <head> 
+        <body>
+        <nav class="navbar-inverse my_nav_bar">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="cllapse" data-traget="#myNavbar">
+                    <span class="icon- bar "></span
+<span class="icon- bar"></span
+<span class="icon- bar"></span
+</button>
+<a class="navbar-brand" href="#">Dr Rajeev Kumar Chauhan</a>
+</div>
+<div class="collapse navbar-collapse" id="myNavbar">
+    <ul class="nav navbar-nav">
+        <li><a herf="#Campus Life">HOME</a></li>
+         <li><a herf="#Achievements">Achievements</a></li>
+           <li><a herf="#"Whats New>Whats New</a></li>  
+              <li><a herf="#"Album>BLOG</a></li>
+                  <li><a herf="#"Contact Us>CONTACT</a></li> 
+                      </ul>
+</div>
+</nav>
+<nav class="navbar-inverse my_nav_bar">
+    <div class="navbar-header">
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FIndian_School%2C_Muscat&psig=AOvVaw2jmVdSEcD_HDG9j7VJF15d&ust=1614413657081000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKj90P2I-e4CFQAAAAAdAAAAABAD">
+  <section id="contact">
+      <iframe src="https://www.google.com/search?source=hp&ei=xa04YN_wNaiPlwTOnpzgDQ&iflsig=AINFCbYAAAAAYDi71WP6zpy911eXQSnIwcdMvB0VWfSM&q=ism+oman&gs_ssp=eJzj4tTP1TewSDIwNzRgtFI1qDBOtTRMszBONE4xSrUwNzK3MqhIS0tLNkkxNUlLMkyysDCx8OLILM5VyM9NzAMA3uMQyA&oq=ism+&gs_lcp=Cgdnd3Mtd2l6EAEYADILCC4QxwEQrwEQkwIyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAA6CAgAEOoCEI8BOggIABCxAxCDAToICC4QsQMQgwE6BQgAELEDOgsILhCxAxCDARCTAjoOCC4QsQMQgwEQxwEQrwE6BQguELEDOhEILhCxAxCDARDHARCvARCTAjoCCC5Q6EJY129gvnpoAnAAeACAAc8CiAGZB5IBBzAuMi4xLjGYAQCgAQGqAQdnd3Mtd2l6sAEC&sclient=gws-wiz#" height="450" frameborder="0" allowfullscreen=""></iframe>
+<div class="contact_1 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+    <a herf="ismoman@omantel.net.om">
+        <h5><i class="fa fa-envelope"></i>&nbsp;&nbsp;ismoman@omantel.net.om</h5>
+            </a>
+  <a herf="#">
+      <h5><i class="fa fa-instagram" ></i>&nbsp;&nbsp;instagramid Indian School Muscut</h5>
+          </a>
+</div>
+</section>
+</body>
+    </html>
